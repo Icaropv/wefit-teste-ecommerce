93c58f338e36c8a9870b5e26f238206499ddb48d
icaropv <icaropaiva0112@gmail.com>
2025-08-24 14:59:31 -0300

    style: ajeitando visualização cartPage

---
+++ wefit-ecommerce/src/pages/Cart.tsx
@@ -0,0 +1,9 @@
+import { CartView } from '../modules/cart/components/CartView';
+
+export function CartPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <CartView />
+    </div>
+  );
+}
