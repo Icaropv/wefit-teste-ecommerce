1ff75fe1d7072adc5d39aaa9e55dc04cb6e6cea9
icaropv <icaropaiva0112@gmail.com>
2025-08-24 14:52:27 -0300

    feat: criação de botao para usos mais simples

--- wefit-ecommerce/src/components/Button/index.tsx
+++ wefit-ecommerce/src/components/Button/index.tsx
@@ -1,38 +1,24 @@
-import React from 'react';
-import carrinho from '../../modules/movies/assets/carrinho.png'
-import { useCart } from '../../modules/cart/hooks/useCart'
-import type { Movie } from '../../types/index'; 
+import React from 'react'
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
-  children: React.ReactNode;
-  movie: Movie
+  children: React.ReactNode,
+complementalClassname?: string
+ 
 
 }
 
-export const ButtonCart = ({ movie }: ButtonProps) => {
-  const { addMovieToCart, getItemQuantity } = useCart();
 
-  const quantityInCart = getItemQuantity(movie.id);
-  const isAdded = quantityInCart > 0;
 
-  const colorClass = isAdded ? 'bg-green-500' : 'bg-[#009EDD]';
-  
+
+
+const Button = ({children, complementalClassname, ...props}: ButtonProps) => {
 
   return (
-    <button
-      onClick={() => addMovieToCart(movie)} 
-      className={`${colorClass} h-[45px] w-[300px] text-white flex justify-center items-center gap-4 rounded-[5px]`}
-    >
-      <div className='flex gap-1 items-center'>
-        <img src={carrinho} alt="" className='h-[20px]' />
-        <p>{quantityInCart}</p> 
-      </div>
-      <div>
-        <p className="text-xs font-bold">Adicionar ao carrinho</p> 
-      </div>
+    <button {...props} className={`h-[45px]  text-white flex justify-center items-center gap-4 rounded-[5px] bg-[#009EDD]  ${complementalClassname}`}>
+        <p className='font-sans font-bold'>{children}</p>
     </button>
-  );
+  )
 }
 
-export default ButtonCart;
+export default Button
