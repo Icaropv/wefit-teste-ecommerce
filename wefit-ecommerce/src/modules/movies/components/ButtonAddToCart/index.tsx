d6340ea10594eff723b3bdff1dcd9d84018fabce
icaropv <icaropaiva0112@gmail.com>
2025-08-24 15:01:32 -0300

    feat: criação de componente de botão complexo, que lida diretamente com hooks

---
+++ wefit-ecommerce/src/modules/movies/components/ButtonAddToCart/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import carrinho from  '../../assets/carrinho.png'
+import { useCart } from '../../../cart/hooks/useCart'
+import type { Movie } from '../../../../types/index'; 
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+
+  children: React.ReactNode;
+  movie: Movie
+
+}
+
+export const ButtonCart = ({ movie }: ButtonProps) => {
+  const { addMovieToCart, getItemQuantity } = useCart();
+
+  const quantityInCart = getItemQuantity(movie.id);
+  const isAdded = quantityInCart > 0;
+
+  const colorClass = isAdded ? 'bg-green-500' : 'bg-[#009EDD]';
+  
+
+  return (
+    <button
+      onClick={() => addMovieToCart(movie)} 
+      className={`${colorClass} h-[45px] w-[300px] text-white flex justify-center items-center gap-4 rounded-[5px]`}
+    >
+      <div className='flex gap-1 items-center'>
+        <img src={carrinho} alt="" className='h-[20px]' />
+        <p>{quantityInCart}</p> 
+      </div>
+      <div>
+        <p className="text-xs font-bold font-sans">ADICIONAR AO CARRINHO</p> 
+      </div>
+    </button>
+  );
+}
+
+export default ButtonCart;
