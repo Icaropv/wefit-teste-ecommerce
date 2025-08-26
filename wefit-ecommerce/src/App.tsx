2cbf52642b7a78e4b1bb8dc48bf673fdc702d7cf
icaropv <icaropaiva0112@gmail.com>
2025-08-26 04:08:04 -0300

    fix(build): resolvendo dependencias de produção

--- wefit-ecommerce/src/App.tsx
+++ wefit-ecommerce/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import HomePage from './pages/HomePage'
 import { Header } from './components/Header'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
